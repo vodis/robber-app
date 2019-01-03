037d0a1eaa441a8c1ea1f78ae17ed7203acf389e
vodis <i40217891@gmail.com>
2019-01-03 17:35:30 +0200

    React was connected to project

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+// class Student {
+//     fullName: string;
+//     constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+//         this.fullName = `${firstName} ${middleInitial} ${lastName}`;
+//     }
+// }
+// interface Person {
+//     firstName: string;
+//     lastName: string;
+// }
+// function greeter(person : Person) {
+//     return `Hello, ${person.firstName} ${person.lastName}`;
+// }
+// const user = new Student('Jane', 'M.', 'User');
+// document.body.innerHTML = greeter(user);
+
+
+import * as React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+
+render(
+    <App message="World" />,
+    document.getElementById('root'),
+);
