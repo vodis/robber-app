19e06ca7dcac01f408db029d5f6c4c57584b7e77
vodis <i40217891@gmail.com>
2019-01-03 16:49:24 +0200

    Init state

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    entry: "./src/index.ts",
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: "awesome-typescript-loader"
+            },
+        ]
+    },
+};
